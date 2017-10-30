334970d9fa29bbcae47f448cd71e60f1937c9dc5
schwibbes <schwibbes2@gmail.com>
2017-10-30 07:28:07 +0100

    move selection logic to syntax scopes WIP

--- Notes.py
+++ Notes.py
@@ -12,40 +12,43 @@ pattern = re.compile("^([A-Z][0-9]+)+$")
 REP_START = '[['
 REP_END = ']]'
 
+SCOPE_TAG = 'entity.name.tag.topic.notes'
+SCOPE_QUERY = 'keyword.query'
+SCOPE_FULL = 'source.block.note'
+SCOPE_COMPACT = 'source.notes.compact'
+SCOPE_REPORT = 'comment.block.report.notes'
+
+settings = sublime.load_settings("Notes.sublime-settings")
+
+
 class ClearReportCommand(sublime_plugin.TextCommand):
 	def run(self,edit):
 		self.clear_views(edit)
 
 	def clear_views(self, edit):
-		regs = self.view.find_by_selector("comment.block")
+		regs = self.view.find_by_selector(SCOPE_REPORT)
 		for reg in regs:
 			self.view.erase(edit, reg)
 
 
 class SubmitCommand(sublime_plugin.TextCommand):
 
-	client = MongoClient("mongodb://192.168.99.100:27017")
-	db = client["test"]
+	client = MongoClient(settings.get("mongo_uri", "mongodb://192.168.99.100:27017"))
+	db = client[settings.get("mongo_db", "mynotes")]
 
+	scopes = [
+		SCOPE_TAG,
+		SCOPE_QUERY,
+		SCOPE_COMPACT,
+		SCOPE_FULL]
+
+	
 	def run(self, edit):
-		self.clearViews(edit)
-		s = self.view.sel()
-		for x in s:
-			print("{:d}, {:d}".format(x.begin() , x.end()))
-
-		strats = [
-			lambda: self.view.substr(s[0]),
-			lambda: self.view.substr(self.view.word(s[0])),
-			lambda: self.view.substr(self.view.line(s[0])),
-			lambda: self.view.substr(self.view.expand_by_class(s[0], sublime.CLASS_EMPTY_LINE)),
-			]
+		for s in self.view.sel():
+			res = [ (scope, self.view.score_selector(s.end(), scope)) for scope in self.scopes ]
+			print(res)
+			max(res,key=lambda item:item[1])[0]
 		
-		for strat in strats:
-			if not self.select(strat(), edit):
-				print("strat worked: " + str(strat().count('\n')))
-				break
-			else:
-				print("retry after strat: " + strat())
 
 	
 	def text2ticket(self,text):
@@ -68,11 +71,6 @@ class SubmitCommand(sublime_plugin.TextCommand):
 		sublime.set_timeout(
 			lambda: self.view.erase_status("savestate"), 2000)
 
-	def clearViews(self, edit):
-		regs = self.view.find_all(REP_START + '.*' + REP_END, 0)
-		for reg in regs:
-			self.view.erase(edit, reg)
-
 	def save(self, text):
 		ticket = self.text2ticket(text)
 		
@@ -80,7 +78,7 @@ class SubmitCommand(sublime_plugin.TextCommand):
 	def print_headline(self, edit, cursor):
 		as_text = ""
 		for doc in cursor:
-			as_text += "*" + str(doc['head']) + " ~" + str(doc['_id']) + "\n"
+			as_text += str(doc['head']) + " ~" + str(doc['_id']) + "\n"
 		
 		blob = self.view.expand_by_class(self.view.sel()[0], sublime.CLASS_EMPTY_LINE)
 		self.view.insert(edit, 
@@ -93,14 +91,14 @@ class SubmitCommand(sublime_plugin.TextCommand):
 
 	def print_ticket(self, edit, ticket):
 		as_text = '\n'.join([
-			"*" + str(ticket['head']),
-			"~" + str(ticket['_id']),
-			'\n'.join(ticket['topics']),
-			'\n'.join(ticket['ts']),
-			'\n'.join(ticket['body'])
+			str(ticket['head']),
+			"  ~" + str(ticket['_id']),
+			'  \n  '.join(ticket['topics']),
+			'\n  '.join(ticket['ts']),
+			'\n  '.join(ticket['body'])
 		])
 		
-		as_text += "\n" + "sha1:" + hashlib.sha1(as_text.encode("utf-8")).hexdigest()
+		as_text += "\n  " + "sha1:" + hashlib.sha1(as_text.encode("utf-8")).hexdigest()
 		
 		current_line = self.view.line(self.view.sel()[0])
 		self.view.replace(
@@ -110,7 +108,7 @@ class SubmitCommand(sublime_plugin.TextCommand):
 
 	def select(self, text, edit):
 		text = text.strip()
-		if text.startswith('*NOTE:'):
+		if text.startswith('NOTE:'):
 			search_res = re.search('.*~([0-9a-f]{24})', text)
 			if search_res:
 				ticket = self.db.notes.find_one({'_id': ObjectId(search_res.group(1)) })
